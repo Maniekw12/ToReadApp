e8ab76b65e1d1038c192318a63eb6a8ec4b03493
Marian Wachala <wachala.marian@gmail.com>
2025-03-18 21:29:12 +0100

    Project initialization

---
+++ src/test/java/com/example/ManieksApp/ManieksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManieksApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManieksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
