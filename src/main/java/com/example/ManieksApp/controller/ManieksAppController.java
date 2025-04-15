8877c97fa7db0aaacf4a629197644685dae18536
Marian Wachala <wachala.marian@gmail.com>
2025-04-15 23:54:36 +0200

    Set up controller

--- src/main/java/com/example/ManieksApp/controller/ManieksAppController.java
+++ src/main/java/com/example/ManieksApp/controller/ManieksAppController.java
@@ -1,7 +1,34 @@
 package com.example.ManieksApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ManieksApp.request.CreateNewBook;
+import com.example.ManieksApp.response.BaseResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ManieksAppController {
+
+    @GetMapping("/books")
+    public BaseResponse getAllBooks(){
+        return new BaseResponse("Okk");
+    }
+
+    @GetMapping("/books/{id}")
+    public BaseResponse getOneBook(@PathVariable Long id){
+        return new BaseResponse("Okk");
+    }
+
+    @DeleteMapping("/books/delete/{id}")
+    public BaseResponse deleteOneBook(@PathVariable Long id){
+        return new BaseResponse("Okk");
+    }
+
+    @PostMapping("/books/post")
+    public BaseResponse postOneBook(@RequestBody CreateNewBook newBook){
+        return new BaseResponse("Okk");
+    }
+
+    @PutMapping("/books/update")
+    public BaseResponse updateOneBook(@RequestBody CreateNewBook newBook){
+        return new BaseResponse("Okk");
+    }
 }
