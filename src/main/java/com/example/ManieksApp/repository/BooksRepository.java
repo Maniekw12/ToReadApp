c47f2d91e2a42f81d14142217d96bbd37570a847
Marian Wachala <wachala.marian@gmail.com>
2025-04-16 12:05:06 +0200

    add PostBook, getAllbooks, getOneBook logic

--- src/main/java/com/example/ManieksApp/repository/BooksRepository.java
+++ src/main/java/com/example/ManieksApp/repository/BooksRepository.java
@@ -3,7 +3,13 @@ package com.example.ManieksApp.repository;
 import com.example.ManieksApp.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BooksRepository extends JpaRepository<BookEntity, Long> {
 
+    Optional<BookEntity> findById(Long id);
+
+    List<BookEntity> findAll();
 
 }
