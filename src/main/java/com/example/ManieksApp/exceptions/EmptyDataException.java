bfad7bc70933a1414f294dda57310083cf75e58b
Marian Wachala <maniekw12w@gmail.com>
2025-04-19 12:47:44 +0200

    change filename from "ValidateDate" to "ValidateData"

--- src/main/java/com/example/ManieksApp/exceptions/EmptyDataException.java
+++ src/main/java/com/example/ManieksApp/exceptions/EmptyDataException.java
@@ -1,7 +1,5 @@
 package com.example.ManieksApp.exceptions;
 
-import com.example.ManieksApp.util.ValidateDate;
-
 public class EmptyDataException extends RuntimeException {
     public EmptyDataException(String message) {
         super(message);
--- src/main/java/com/example/ManieksApp/util/ValidateData.java
+++ src/main/java/com/example/ManieksApp/util/ValidateData.java
@@ -2,7 +2,7 @@ package com.example.ManieksApp.util;
 
 import com.example.ManieksApp.exceptions.EmptyDataException;
 
-public class ValidateDate {
+public class ValidateData {
     public static void validateName(String Name) {
         if(Name == null || Name.isEmpty()){
             throw new EmptyDataException("Name cannot be null or empty");
